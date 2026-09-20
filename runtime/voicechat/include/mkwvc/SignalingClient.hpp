6b3f0a06f9345e22dde887902dbc8699e6ede0a1
waxed <119123752+waxedhyphen@users.noreply.github.com>
2026-09-20 22:48:42 +0200

    Stage existing MKW VoiceChat signaling header

---
+++ runtime/voicechat/include/mkwvc/SignalingClient.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace mkwvc {
+
+enum class SignalingEventType {
+    Open,
+    RoomCreated,
+    RoomJoined,
+    RoomLeft,
+    IceServers,
+    PeerReady,
+    PeerInfo,
+    Signal,
+    PeerLeft,
+    RetroRewindStatus,
+    RetroRewindDebugStatus,
+    RetroRewindDebugFailed,
+    RetroRewindAuthFailed,
+    RetroRewindAuthRequired,
+    Error,
+    TransportError,
+    Closed
+};
+
+struct SignalingEvent {
+    SignalingEventType type;
+    std::string payload;
+    std::string memberId;
+};
+
+class SignalingClient {
+public:
+    explicit SignalingClient(std::string serverUrl);
+    ~SignalingClient();
+
+    SignalingClient(const SignalingClient&)=delete;
+    SignalingClient& operator=(const SignalingClient&)=delete;
+
+    void setRoomNone();
+    void setRoomCreate(std::string memberId,std::string displayName);
+    void setRoomJoin(std::string roomCode,std::string memberId,std::string displayName);
+    void sendSignal(std::string signal);
+    void sendSignal(std::string memberId,std::string signal);
+    void authenticateRetroRewind(std::string profileId,std::string sessionKey,std::string gameName);
+    void debugLookupRetroRewind(std::string profileId);
+    void setDebugRetroRewindPresence(std::string profileId);
+    void clearDebugRetroRewindPresence();
+    void syncRetroRewindRoom();
+    bool open() const;
+    void service();
+    std::vector<SignalingEvent> takeEvents();
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> impl_;
+};
+
+}
